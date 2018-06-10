84ce036f3dd1e027514465af5f22b1dcece78921
lxuserb <zalewmic@gmail.com>
2018-06-10 15:42:04 +0200

    convert a number to its binary representation with Deque

--- src/main/java/com/myself/arithmetics/binaryconversion/App.java
+++ src/main/java/com/myself/arithmetics/binaryconversion/App.java
@@ -1,11 +1,19 @@
 package com.myself.arithmetics.binaryconversion;
 
 
+import com.google.common.primitives.Ints;
+
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class App {
 
     public static void main(String[] args) {
-        for(int i = 0; i < 33; i++)
+        for(int i = 0; i <= 16; i++)
             System.out.println(String.format("%3d as binary is: %6s",i, convertToBinaryString(i)));
+        for(int i = 0; i <= 16; i++)
+            System.out.println(String.format("%3d as binary is: %16s",i, Arrays.toString(convertToBinary(i))));
+
     }
     public static String convertToBinaryString(int number) {
         String result = "";
@@ -13,5 +21,11 @@ public class App {
         for(int d = number; d > 0; d /= 2) // d=d/2
             result = (d % 2) + result;
         return (result.equals("") ? "0" : result);
+    }
+    public static int[] convertToBinary(int number) {
+        Deque<Integer> result = new LinkedList<>();
+        for(int d = number; d > 0; d /= 2) // d=d/2
+            result.addFirst(d % 2);
+        return Ints.toArray(result);
     }
 }
