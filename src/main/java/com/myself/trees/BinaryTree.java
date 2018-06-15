7ea511348c2ee1790570636814b5a8dec3f60d45
lxuserb <zalewmic@gmail.com>
2018-06-15 21:40:08 +0200

    evaluation of postfix expressions

---
+++ src/main/java/com/myself/trees/BinaryTree.java
@@ -0,0 +1,7 @@
+package com.myself.trees;
+
+/**
+ * Created by lxuserb on 15.06.18.
+ */
+public class BinaryTree {
+}
---
+++ src/main/java/com/myself/trees/bds/App.java
@@ -0,0 +1,7 @@
+package com.myself.trees.bds;
+
+/**
+ * Created by lxuserb on 15.06.18.
+ */
+public class App {
+}
